37f779ea82d8b9c4288e1a099d568002be395d23
Caroline-Patrick <carolinespatrick@gmail.com>
2022-10-20 11:22:17 -0500

    added ramp functions

--- main.js
+++ main.js
@@ -27,7 +27,36 @@ const hangmanFunc = (guess) => {
 
 // Challenge: Given the input of a number, build a program that will find the total number of ramp numbers that are less than it.
 
-
+// first function determines whether or not a number is a ramp. it turns the number into an array so that it can be looped through. if any succeeding number is higher than the prior number, then failCount will no longer = 0. If it is a ramp #, the function will return 'true'
+const isRamp = (num) => {
+    let failCount =0
+    let numArr = num.toString().split('')  
+    for (let i = 0; i < numArr.length; i++) {
+      
+      if(numArr[i]> numArr[i+1]){
+        failCount++
+      }
+    }
+    if(failCount == 0){
+    return true
+    }
+  }
+  
+  // this function iterates through the given number (in our example 99999), and each iterator is then put into the isRamp function to determine if it is a ramp # or not. If the return of the isRamp comes back as true, we add 1 to the ramp count. 
+  const rampCounter = (num) => {
+    let iterator = 0
+    let rampCount = 0
+    while (iterator < num) {
+      iterator++
+      isRamp(iterator)
+      if (isRamp(iterator) === true) {
+        rampCount++
+      }
+    }
+  console.log('rampCount is: ' + rampCount)
+    return rampCount
+  }
+  
 
 
 // -----
